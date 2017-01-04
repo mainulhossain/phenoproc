8b9cd6c406d2fcbee7c2f9ba45c7bf9c37bcb614
Mainul Hossain <mainulhossain@gmail.com>
2017-01-04 22:59:07 +0600

    task-manager use

--- app/operations/hadoop.py
+++ app/operations/hadoop.py
@@ -6,18 +6,19 @@ from subprocess import call
 import uuid
 from . import spawn
 from flask import current_app
+from .tasks import task_manager
 
-def runHadoop(mapper, reducer, cwd, input, output):
+def runHadoop(task_id, mapper, reducer, cwd, input, output):
         hadoopPath = '/usr/bin/hadoop'
         streamPath = '/usr/hdp/2.5.0.0-1245/hadoop-mapreduce/hadoop-streaming-2.7.3.2.5.0.0-1245.jar'
 #       call([hadoopPath, 'jar "%s"' % streamPath, '-files mapper-is.py, -libjars WholeFileInputFormat.jar,NamedFileOutputFormat.jar -D mapreduce.input.fileinputformat.input.dir.recursive=true -io rawbytes -mapper "mapper-is.py %s" -input "%s" -output "%s" -inputformat WholeFileInputFormat -outputformat NamedFileOutputFormat' % (src, input, output)])
         #v = 'jar ' + streamPath + ' -files ' + mapper + ',' + reducer + '-D mapreduce.input.fileinputformat.input.dir.recursive=true' + ' -mapper ' + mapper + ' -reducer ' + reducer + -input  -output output'
         jobid = str(uuid.uuid4())
-        v = 'jar {0} -files {1},{2} -D mapreduce.input.fileinputformat.input.dir.recursive=true|mapred.job.name="{3}" -mapper {4} -reducer {5} -input {6} -output {7}'.format(streamPath, mapper, reducer, jobid, os.path.basename(mapper), os.path.basename(reducer), input, output)
-        print(v, file=sys.stderr)
+        args = 'jar {0} -files {1},{2} -D mapreduce.input.fileinputformat.input.dir.recursive=true|mapred.job.name="{3}" -mapper {4} -reducer {5} -input {6} -output {7}'.format(streamPath, mapper, reducer, jobid, os.path.basename(mapper), os.path.basename(reducer), input, output)
+        #print(v, file=sys.stderr)
         #call([hadoopPath, v])
-        spawn.execute([current_app.config['WEBHDFS_USER'], cwd, hadoopPath, v])
-
+        #spawn.execute([current_app.config['WEBHDFS_USER'], cwd, hadoopPath, v])
+        task_manager.submit(task_id, [hadoopPath, args])
 
 if __name__ == "__main__":
         runHadoop(sys.argv[1], sys.argv[2], sys.argv[3])
--- app/operations/wordcount/action.py
+++ app/operations/wordcount/action.py
@@ -1,8 +1,11 @@
-from ...models import Workflow, WorkItem, DataSource, OperationSource
+from __future__ import print_function
+
+from ...models import Workflow, WorkItem, DataSource, OperationSource, Task
 import sys
 from subprocess import call
 from ..hadoop import runHadoop
 import os
+from ..tasks import task_manager
 
 #wordcount = Blueprint('wordcount', __name__)
 class Action(object):
@@ -18,12 +21,18 @@ class Action(object):
     def run_hdfs(workitem):
         input_datasource = DataSource.query.get(workitem.inputs.datasource_id)
         output_datasource = DataSource.query.get(workitem.outputs.datasource_id)
-        runHadoop(os.path.abspath('mapper.py'), os.path.abspath('reducer.py'), os.path.abspath(os.path.dirname(__file__)), os.path.join(input_datasource.url, workitem.inputs.get_fullpath()), os.path.join(output_datasource.url, workitem.outputs.get_fullpath()))
+        task_id = Task.create_task(workitem.id)
+        runHadoop(task_id, os.path.abspath('mapper.py'), os.path.abspath('reducer.py'), os.path.abspath(os.path.dirname(__file__)), os.path.join(input_datasource.url, workitem.inputs.get_fullpath()), os.path.join(output_datasource.url, workitem.outputs.get_fullpath()))
     
     @staticmethod
     def run_fs(workitem):
-        sys.stdin = open('*.txt')
-        import mapper.py
-        outputFile =''
-        with open('outputFile', 'w') as f:
-            call(['python', 'mapper.py'], stdout=f)
+        task_id = Task.create_task(workitem.id)
+        task_manager.submit(task_id, ['ls', '-l'])
+        
+#     @staticmethod
+#     def run_fs(workitem):
+#         sys.stdin = open('*.txt')
+#         import mapper.py
+#         outputFile =''
+#         with open('outputFile', 'w') as f:
+#             call(['python', 'mapper.py'], stdout=f)
