12d219085801e4e8f63ae770436b37f49ed324f9
Mainul Hossain <mainulhossain@gmail.com>
2017-01-05 18:20:09 +0600

    task updated

--- app/operations/tasks.py
+++ app/operations/tasks.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
 from concurrent.futures import ThreadPoolExecutor
-from subprocess import call
+from subprocess import call, check_output
 from past.builtins.misc import execfile
-from ..models import Task, TaskStatus
+from ..models import Task, TaskStatus, TaskLog
+from .. import db
+from sqlalchemy import func
 
 import sys
 
@@ -14,11 +16,31 @@ class TaskManager:
     def submit(self, task_id, argv):
         execfile = argv[:1]
         args = argv[1:]
-        self.futures = {'task_id': task_id, 'future': self.pool.submit(call, [execfile, args])}
+        self.futures = {task_id: self.pool.submit(check_output, argv)}
         task = Task.query.get(task_id)        
         task.add_log(TaskStatus.Running)
     
     def check_task(self, task_id):
+        status = self.manage_running_task(task_id)
+        if status is None:
+            logs = db.session.query(TaskLog.status, func.max(TaskLog.time)).filter(TaskLog.task_id==task_id).group_by(TaskLog.task_id)
+            if logs:
+                status = logs.first().status
+        return status
+    
+    def manage_running_task(self, task_id):
+        status = self.managed_task_status(task_id)
+        if status is not None:
+            Task.query.get(task_id).add_log(status)
+            if status is not TaskStatus.Running:
+                del self.futures[task_id]
+        return status;         
+    
+    def managed_tasks_to_tasklogs(self):
+        for t in self.futures:
+            self.check_running_task(t)
+            
+    def managed_task_status(self, task_id):
         future = self.futures[task_id]
         status = TaskStatus.Unknown
         if future is not None:
@@ -28,8 +50,7 @@ class TaskManager:
                 status = TaskStatus.Running
             elif future.done():
                 status = TaskStatus.Compeleted
-        if status is not None:
-            Task.query.get(task_id).add_log(status)
-        return status;         
-    
+                #output = future.result()
+        return status
+            
 task_manager = TaskManager()
