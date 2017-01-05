384e1c3142210f7a30904f035fea83f2d24c7f72
Mainul Hossain <mainulhossain@gmail.com>
2017-01-06 02:01:21 +0600

    task as a single string instead of a list

--- app/operations/tasks.py
+++ app/operations/tasks.py
@@ -16,7 +16,9 @@ class TaskManager:
     def submit(self, task_id, argv):
         execfile = argv[:1]
         args = argv[1:]
-        self.futures = {task_id: self.pool.submit(check_output, argv, shell=True)}
+        #self.futures = {task_id: self.pool.submit(check_output, argv, shell=True)}
+        print(' '.join(argv))
+        self.futures = {task_id: self.pool.submit(check_output, ' '.join(argv), shell=True)}
         task = Task.query.get(task_id)        
         task.add_log(TaskStatus.Running)
     
