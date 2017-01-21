a2d0980a4cf51058a929478fe6a9c6ee38e07fb4
Mainul Hossain <mainulhossain@gmail.com>
2017-01-21 13:35:47 +0600

    mapper adjustment to python 3.5

--- app/operations/wordcount/mapper.py
+++ app/operations/wordcount/mapper.py
@@ -16,7 +16,7 @@ for line in sys.stdin:
             # Reduce step, i.e. the input for reducer.py
             #
             # tab-delimited; the trivial word count is 1
-            print '%s\t%s' % (word, 1)
+            print('{0}\t{1}'.format(word, 1))
     except:
         Pass
 
