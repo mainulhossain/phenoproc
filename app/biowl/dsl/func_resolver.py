cabe230cca8392d33fd84ff35e8d62d02b8abeb4
Mainul Hossain <mainulhossain@gmail.com>
2018-01-22 19:25:11 +0600

    galaxy add history_id

--- app/biowl/dsl/func_resolver.py
+++ app/biowl/dsl/func_resolver.py
@@ -211,7 +211,7 @@ class Library():
         
         # special handling for galaxy if history_id is not given, use history id from symbol table
         if (func[0].module == 'app.biowl.libraries.galaxy.adapter'):
-            if not history_id in kwargs and context.var_exists('history_id'):
+            if not 'history_id' in kwargs and context.var_exists('history_id'):
                 kwargs['history_id'] = context.get_var('history_id')
         return function(*arguments, **kwargs)
 
