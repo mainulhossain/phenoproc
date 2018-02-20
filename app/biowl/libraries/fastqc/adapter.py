5bd3d90851cd5ef602a0631025ad6505904e4e70
Mainul Hossain <mainulhossain@gmail.com>
2018-02-20 11:20:47 +0600

    fastqc

--- app/biowl/libraries/fastqc/adapter.py
+++ app/biowl/libraries/fastqc/adapter.py
@@ -1,33 +1,54 @@
 import os
 from os import path
 from ...exechelper import func_exec_run
-from ...fileop import IOHelper, PosixFileSystem
+from ...fileop import PosixFileSystem
 from ....util import Utility
 
 fastqc = path.join(path.abspath(path.dirname(__file__)), path.join('lib', 'fastqc'))
 
-def run_fastqc(*args):
-    fs = PosixFileSystem(Utility.get_rootdir(2))
-    input = fs.normalize_path(Utility.get_quota_path(args[0]))
-    cmdargs = [input]
-    outdir = path.dirname(input)
-    if len(args) > 1:
-        outdir = fs.normalize_path(Utility.get_quota_path(args[1]))
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-    cmdargs.append("--outdir=" + outdir)
-        
+def run_fastqc(*args, **kwargs):
+    
+    paramindex = 0
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        if len(args) == paramindex:
+            raise ValueError("Argument missing error in FastQC.")
+        data = args[paramindex]
+        paramindex +=1
+    
+    data = Utility.get_normalized_path(data)
+    
+    if 'outdir' in kwargs.keys():
+        outdir = kwargs['outdir']
+    else:
+        if len(args) > paramindex:
+            outdir = args[paramindex]
+            paramindex +=1
+    
+    if outdir:
+        outdir = Utility.get_normalized_path(outdir)
+    else:
+        outdir = path.dirname(data)
+    
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
+    cmdargs = [data, "--outdir=" + outdir]
+                       
     for arg in args[2:]:
         cmdargs.append(arg)
     
-    outname = path.basename(input)
-    outname = outname.split(os.extsep)[0] + "_fastqc.html"
-    outpath = path.join(outdir, outname)
-    
-    func_exec_run(fastqc, *cmdargs)
-    
+    _,err = func_exec_run(fastqc, *cmdargs)
+
+    output = path.basename(data)
+    output = output.split(os.extsep)[0] + "_fastqc.html"
+    outpath = path.join(outdir, output)
+        
+    fs = PosixFileSystem(Utility.get_rootdir(2))
     stripped_path = fs.strip_root(outpath)
     if not os.path.exists(stripped_path):
-        raise ValueError("FastQC could not generate the file " + stripped_path)
+        raise ValueError("FastQC could not generate the file " + stripped_path + " due to error " + err)
+    
     return stripped_path
     
