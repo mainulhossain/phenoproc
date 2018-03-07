908a6779d8e6a30cad1d2f4359b19f4568798105
Mainul Hossain <mainulhossain@gmail.com>
2018-03-07 16:16:44 +0600

    bowtie2

--- app/biowl/libraries/bowtie2/adapter.py
+++ app/biowl/libraries/bowtie2/adapter.py
@@ -82,7 +82,7 @@ def run_bwa(*args, **kwargs):
     for arg in args[paramindex + 1:]:
         cmdargs.append(arg)
     
-    _,err = func_exec_run(bwa, *cmdargs)
+    _,err = func_exec_run(bowtie2, *cmdargs)
     
     fs = PosixFileSystem(Utility.get_rootdir(2))
     if not os.path.exists(output):
