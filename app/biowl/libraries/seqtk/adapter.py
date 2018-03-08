c94854442a193799e65d67a2d4a10fd9c5b27761
Mainul Hossain <mainulhossain@gmail.com>
2018-03-08 12:48:54 +0600

    seqtk

--- app/biowl/libraries/seqtk/adapter.py
+++ app/biowl/libraries/seqtk/adapter.py
@@ -6,14 +6,11 @@ from ....util import Utility
 
 seqtk = path.join(path.abspath(path.dirname(__file__)), path.join('bin', 'seqtk'))
 
-def run_seqtk(*args):
-    fs = PosixFileSystem(Utility.get_rootdir(2))
-
-    input = fs.normalize_path(Utility.get_quota_path(args[0]))
-    command = args[1]
-    output = fs.normalize_path(Utility.get_quota_path(args[2]))
+def run_seqtk(*args, **kwargs):
+    input = Utility.get_normalized_path(args[0])
+    output = Utility.get_normalized_path(args[2])
     
-    cmdargs = [command]
+    cmdargs = [args[1]]
     for arg in args[3:]:
         cmdargs.append(arg)
             
@@ -22,38 +19,68 @@ def run_seqtk(*args):
     outdata,_ = func_exec_stdout(seqtk, *cmdargs)
     with open(output, 'wb') as f:
         f.write(outdata)
+        
+    fs = PosixFileSystem(Utility.get_rootdir(2))
     return fs.strip_root(output)
 
-def seqtk_fastq_to_fasta(*args):
-    fs = PosixFileSystem(Utility.get_rootdir(2))
+def seqtk_fastq_to_fasta(*args, **kwargs):
+    paramindex = 0
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        if len(args) == paramindex:
+            raise ValueError("Argument not given.")
+        data = args[paramindex]
+        paramindex += 1
+                
+    if 'output' in kwargs.keys():
+        output = kwargs['output']
+    else:
+        if len(args) > paramindex:
+            output = args[paramindex]
+            paramindex += 1
 
-    input = fs.normalize_path(Utility.get_quota_path(args[0]))
-    output = fs.normalize_path(Utility.get_quota_path(args[1]))
-    
     cmdargs = [input, 'seq -a', output]
-    for arg in args[2:]:
+    for arg in args[paramindex + 1:]:
         cmdargs.append(arg)
     return run_seqtk(*cmdargs)
 
-def seqtk_extract_sample(*args):
-    fs = PosixFileSystem(Utility.get_rootdir(2))
-
-    input = fs.normalize_path(Utility.get_quota_path(args[0]))
-    output = fs.normalize_path(Utility.get_quota_path(args[1]))
+def seqtk_extract_sample(*args, **kwargs):
+    paramindex = 0
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        if len(args) == paramindex:
+            raise ValueError("Argument not given.")
+        data = args[paramindex]
+        paramindex += 1
+                
+    if 'output' in kwargs.keys():
+        output = kwargs['output']
+    else:
+        if len(args) > paramindex:
+            output = args[paramindex]
+            paramindex += 1
     
-    cmdargs = ['sample']
-    if len(args) > 3:
-        cmdargs.append('-s ' + str(args[3]))
+    if 'sample' in kwargs.keys():
+        sample = kwargs['sample']
+    else:
+        if len(args) > paramindex:
+            sample = args[paramindex]
+            paramindex += 1
+        else:
+            sample = 10
+            
+    cmdargs = ['sample -s {0}'.format(sample)]
     
-    cmdargs.append(input)
-    cmdargs.append(str(args[2]))
+    cmdargs.extend([input, str(args[2])])
 
     outdata,_ = func_exec_stdout(seqtk, *cmdargs)
     with open(output, 'wb') as f:
         f.write(outdata)
     return fs.strip_root(output)
 
-def seqtk_trim(*args):
+def seqtk_trim(*args, **kwargs):
     fs = PosixFileSystem(Utility.get_rootdir(2))
 
     input = fs.normalize_path(Utility.get_quota_path(args[0]))
